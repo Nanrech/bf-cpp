a9afac469c1f186a4eff092792e91ec919b903f7
Nan R <88463477+Nanrech@users.noreply.github.com>
2024-05-15 01:51:03 +0200

    Move function bodies back to .cpp file

--- include/interpreter.h
+++ include/interpreter.h
@@ -19,86 +19,14 @@ class BfInterpreter {
     vector<char> tape = {0};  // Unlimited length tape
     unsigned int tape_pointer = 0;     // Current cell in tape
 
-    void move_right() {
-      // Move .amount to the right. If the tape is too small, expand it
-      unsigned int token_amount = tokens[program_pointer].amount;
-
-      if ((tape_pointer + token_amount) >= tape.size()) {
-        for (unsigned int _ = 0; _ < token_amount; _++) {
-          tape_pointer++;
-          tape.push_back(0);
-        }
-      }
-      else {
-        for (unsigned int _ = 0; _ < token_amount; _++) {
-          tape_pointer++;
-        }
-      }
-
-      program_pointer++;
-    }
-
-    void move_left() {
-      // Move .amount to the left. No negative index allowed
-      unsigned int token_amount = tokens[program_pointer].amount;
-      // "error: cOmPaRiSoN oF uNsIgNeD eXpReSsIoN iN '>= 0' iS aLwAyS tRuE" 🤓☝
-      int diff = tape_pointer - token_amount;
-
-      if (diff >= 0) {
-        tape_pointer -= token_amount;
-      }
-      else {
-        tape_pointer = 0;
-      }
-
-      program_pointer++;
-    }
-
-    void increment() {
-      tape[tape_pointer] += tokens[program_pointer].amount;
-      program_pointer++;
-    }
-
-    void decrement() {
-      tape[tape_pointer] -= tokens[program_pointer].amount;
-      program_pointer++;
-    }
-
-    void output() {
-      for (unsigned int _ = 0; _ < tokens[program_pointer].amount; _++) {
-        cout << tape[tape_pointer];
-      }
-      program_pointer++;
-    }
-
-    void input() {
-      // No idea why this (more than one consecutive ,) would ever be used but it is supported
-      for (unsigned int _ = 0; _ < tokens[program_pointer].amount; _++) {
-        char c;
-        cin >> c;
-        tape[tape_pointer] = c;
-      }
-
-      program_pointer++;
-    }
-
-    void bracket_open() {
-      if (tape[tape_pointer] == 0) {
-        program_pointer = tokens[program_pointer].amount + 1;
-      }
-      else {
-        program_pointer++;
-      }
-    }
-
-    void bracket_close() {
-      if (tape[tape_pointer] != 0) {
-        program_pointer = tokens[program_pointer].amount + 1;
-      }
-      else {
-        program_pointer++;
-      }
-    }
+    void move_right();
+    void move_left();
+    void increment();
+    void decrement();
+    void output();
+    void input();
+    void bracket_open();
+    void bracket_close();
 
   public:
     vector<BfToken> tokens; // No encapsulation. I'm taking the tokens from main and inserting them into this thing RAW.
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -68,3 +68,84 @@ void BfInterpreter::run() {
     }
   }
 }
+
+void BfInterpreter::move_right() {
+  // Move .amount to the right. If the tape is too small, expand it
+  unsigned int token_amount = tokens[program_pointer].amount;
+
+  if ((tape_pointer + token_amount) >= tape.size()) {
+    for (unsigned int _ = 0; _ < token_amount; _++) {
+      tape_pointer++;
+      tape.push_back(0);
+    }
+  }
+  else {
+    for (unsigned int _ = 0; _ < token_amount; _++) {
+      tape_pointer++;
+    }
+  }
+
+  program_pointer++;
+}
+
+void BfInterpreter::move_left() {
+  // Move .amount to the left. No negative index allowed
+  unsigned int token_amount = tokens[program_pointer].amount;
+  // "error: cOmPaRiSoN oF uNsIgNeD eXpReSsIoN iN '>= 0' iS aLwAyS tRuE" 🤓☝
+  int diff = tape_pointer - token_amount;
+
+  if (diff >= 0) {
+    tape_pointer -= token_amount;
+  }
+  else {
+    tape_pointer = 0;
+  }
+
+  program_pointer++;
+}
+
+void BfInterpreter::increment() {
+  tape[tape_pointer] += tokens[program_pointer].amount;
+  program_pointer++;
+}
+
+void BfInterpreter::decrement() {
+  tape[tape_pointer] -= tokens[program_pointer].amount;
+  program_pointer++;
+}
+
+void BfInterpreter::output() {
+  for (unsigned int _ = 0; _ < tokens[program_pointer].amount; _++) {
+    cout << tape[tape_pointer];
+  }
+  program_pointer++;
+}
+
+void BfInterpreter::input() {
+  // No idea why this (more than one consecutive ,) would ever be used but it is supported
+  for (unsigned int _ = 0; _ < tokens[program_pointer].amount; _++) {
+    char c;
+    cin >> c;
+    tape[tape_pointer] = c;
+  }
+
+  program_pointer++;
+}
+
+void BfInterpreter::bracket_open() {
+  if (tape[tape_pointer] == 0) {
+    program_pointer = tokens[program_pointer].amount + 1;
+  }
+  else {
+    program_pointer++;
+  }
+}
+
+void BfInterpreter::bracket_close() {
+  if (tape[tape_pointer] != 0) {
+    program_pointer = tokens[program_pointer].amount + 1;
+  }
+  else {
+    program_pointer++;
+  }
+}
