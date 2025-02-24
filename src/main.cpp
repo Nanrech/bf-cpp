a475a34436eb22fc5d4b704aab6fe7d4168b0a21
Nan R <88463477+Nanrech@users.noreply.github.com>
2025-02-24 18:25:25 +0100

    Complete remake

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,153 @@
+#include <cstdint>
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <stack>
-#include "interpreter.h"
+#include <array>
 
-using namespace std;
+#define MAX_TAPE_SIZE 30000
+#define IS_CHAR_VALID(c) (c == '>' || c == '<' || c == '+' || c == '-' || c == '.' || c == ',' || c == '[' || c == ']')
+
+typedef struct Token {
+  char type;
+  int data; // index to matching jump instruction
+} Token;
 
 
 int main(int argc, char* argv[]) {
-  // Don't look here, it's ugly
+  // Input file provided?
   if (argc != 2) {
-    cout << "Usage: " << argv[0] << " <program.bf>\nMissing bf file." << endl;
-    return 1;
-  }
-  // Standard error checking
-  ifstream in_file(argv[1]);
-  if (in_file.fail()) {
-    cout << "Error reading " << argv[1] << endl;
+    std::cout << "Error: No input file.\nUsage: " << argv[0] << " <program.bf>" << std::endl;
     return 1;
   }
 
-  // ---- Parser happens below this point ----
+  // Input file exists?
+  std::ifstream inFile(argv[1]);
 
-  BfInterpreter interpreter;
-  stack<unsigned int> bracket_stack;  // Keeps track of previous brackets
-  char c;                             // char of holding
-  unsigned int total_tokens = 0;
+  if (inFile.fail()) {
+    std::cout << "Error: Couldn't read input file " << argv[1] << std::endl;
+    return 1;
+  }
 
-  while (!in_file.get(c).eof()) {
-    char buffer_character = c;
-    unsigned int single_token_amount = 1;
+  // ---- Read file ----
+  // First pass, calculate amount of tokens
+  char c;
+  int tokenAmount = 0;
 
-    if (IS_IO_CHAR(c)) {
-      interpreter.tokens.push_back(BfToken {.amount = single_token_amount, .type = buffer_character});
-      total_tokens++;
-    }
-    else if (IS_MULTI_OPERATOR_CHAR(c)) {
-      while (!in_file.get(c).eof()) {
-        if (!IS_VALID_CHAR(c)) {
-          // Text, whitespace, whatever. Don't need it. Skip.
-          continue;
-        }
-        else {
-          if (c == buffer_character) {
-            // Minor optimization. The parser merges consecutive equal tokens into one
-            // So ++++++++ would be one {'+', 8} token instead of 8 '+' tokens
-            single_token_amount++;
-            continue;
-          }
-          else {
-            // Put it back where it came from
-            in_file.putback(c);
-            // And insert what we must
-            interpreter.tokens.push_back(BfToken {.amount = single_token_amount, .type = buffer_character});
-            total_tokens++;
-            break;
-          }
-        }
-      }
-      if (in_file.eof()) {
-        interpreter.tokens.push_back(BfToken {.amount = single_token_amount, .type = buffer_character});
-        total_tokens++;
-      }
-    }
-    else if (IS_BRACKET_CHAR(c)) {
-      if (c == '[') {
-        BfToken new_token = {
-          .amount = single_token_amount,
-          .type = buffer_character
-        };
-        interpreter.tokens.push_back(new_token);
-        bracket_stack.push(total_tokens);
-        total_tokens++;
-      }
-      else {
-        if (bracket_stack.empty()) {
-          cout << "Invalid syntax (bracket mismatch)" << endl;
-          exit(-1);
-        }
-        BfToken new_token = {
-          .amount = bracket_stack.top(),
-          .type = c
-        };
-        interpreter.tokens[bracket_stack.top()].amount = total_tokens;
-        interpreter.tokens.push_back(new_token);
-        bracket_stack.pop();
-        total_tokens++;
-      }
+  while (inFile.get(c)) {
+    if (IS_CHAR_VALID(c)) {
+      tokenAmount++;
     }
   }
-  in_file.close();
 
-  // Done!
-  interpreter.run();
+  // ---- init 'ROM' ----
+  std::vector<Token> commands;
+  commands.resize(tokenAmount);
+
+  // Rewind file
+  inFile.clear();
+  inFile.seekg(0);
+
+  // ---- Store tokens ----
+  // Second pass, store tokens
+  int i = 0;
+  std::stack<int> bracketStack;
+
+  while (inFile.get(c)) {
+    if (!IS_CHAR_VALID(c)) { continue; }
+
+    int data = 0;
+
+    if (c == '[') {
+      // Store last opening bracket
+      bracketStack.push(i);
+    }
+    else if (c == ']') {
+      // Check for underflow
+      if (bracketStack.size() == 0) {
+        std::cout << "Error in input file at pos. " << inFile.tellg() << std::endl;
+        throw std::runtime_error("Unmatched Closing Bracket");
+      }
+
+      // Assign latest opening bracket and pop the stack
+      data = bracketStack.top();
+      bracketStack.pop();
+      // Assign current index to latest opening bracket
+      commands[data].data = i;
+    }
+
+    commands[i++] = Token {
+      .type = c,
+      .data = data,
+    };
+  }
+
+  // Check for overflow
+  if (bracketStack.size() != 0) {
+    std::cout << "Error in input file" << std::endl;
+    throw std::runtime_error("Unmatched Opening Bracket");
+  }
+
+  // ---- Close file ----
+  inFile.close();
+
+  // ---- init memory ----
+  std::array<uint8_t, MAX_TAPE_SIZE> tape = {0};
+  size_t pointer = 0;
+
+  // ---- Execute ----
+  unsigned int idx = 0;
+
+  while (idx != commands.size()) {
+    auto t = commands[idx];
+
+    switch (t.type) {
+      case '>': {
+        if (pointer == tape.size() - 1) { pointer = 0; }
+        else { pointer++; }
+        break;
+      }
+      case '<': {
+        if (pointer == 0) { pointer = tape.size() - 1; }
+        else { pointer--; }
+        break;
+      }
+      case '+': {
+        tape[pointer]++;
+        break;
+      }
+      case '-': {
+        tape[pointer]--;
+        break;
+      }
+      case '.': {
+        // TODO: Do better
+        std::cout << tape[pointer];
+        break;
+      }
+      case ',': {
+        // TODO: Do better
+        std::cin >> tape[pointer];
+        break;
+      }
+      case '[': {
+        if (tape[pointer] == 0) {
+          idx = t.data;
+        }
+        break;
+      }
+      case ']': {
+        if (tape[pointer] != 0) {
+          idx = t.data;
+        }
+        break;
+      }
+      default:
+        break;
+    }
+
+    idx++;
+  }
 
   return 0;
 }
